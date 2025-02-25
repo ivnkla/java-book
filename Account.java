4cc8e175d2fd6bbd9074c3e247452f86d59c8b42
Kalvin-Giang Pichon <kapi@eduroam-113176.grenet.fr>
2025-02-25 13:23:07 +0100

    Ajout de pas mal de choses : - fonction d'affichage - modification de la fonction de création de compte - un seul scanner défini dans le main, passé en argument dans les fonctions - libération du buffer avec nextLine()

--- Account.java
+++ Account.java
@@ -1,15 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Account {
+    Integer id;
     String type;
     Boolean savings;
-    Integer id;
     Integer first_dep;
     Integer rate;
 
-    //Create an account
-    public void create_account(){
-        Scanner scanner = new Scanner(System.in);
+    private void create(Scanner scanner){
         //type
         System.out.println("Type du compte [Types possibles : courant, joint, épargne] :");
         String type = scanner.nextLine();
@@ -45,49 +45,64 @@ public class Account {
             Integer rate = scanner.nextInt();
             this.rate = rate;
         }
-        
     }
 
-    //Show data
-    public void show_account(){
-        System.out.println("Type du compte : " + this.type);
-        System.out.println("Valeur courante : ");
-        System.out.println("Taux de placement si compte épargne :");
-
+    private void display() {
+        System.out.println("id : " + this.id);
+        System.out.println("type : " + this.type);
+        if (this.savings == true) {
+            System.out.println("taux : " + this.rate);
+        }
+        System.out.println("premier dépoôt : " + this.first_dep);
     }
+
     public static void main (String [] args) {
 
-        
+        Map<Integer, Account> accounts = new HashMap<>();
 
-
-        //Menu
-        System.out.println("1. Créer un compte");
-        System.err.println("2. Afficher un compte");
-        System.err.println("3. Créer une ligne comptable");
-        System.err.println("4. Sortir");
-        System.err.println("5. De l'aide");
         
         Scanner scanner = new Scanner(System.in);
-        Integer lu = scanner.nextInt();
+        Integer lu;
         
-        switch (lu) {
-            case 1:
-                Account.create_account();
-                break;
-            case 2:
+        //Menu
+        while(true) {
+            System.out.println("1. Créer un compte");
+            System.err.println("2. Afficher un compte");
+            System.err.println("3. Créer une ligne comptable");
+            System.err.println("4. Sortir");
+            System.err.println("5. De l'aide");
 
-                break;
-            case 3:
+            lu = scanner.nextInt();
+            scanner.nextLine();
 
-                break;
-            case 4:
+            switch (lu) {
+                case 1:
+                    Account account = new Account();
+                    account.create(scanner);
+                    accounts.put(account.id, account);
+                    scanner.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Saisissez un numéro de compte");
+                    Integer id = scanner.nextInt();
+                    if (accounts.containsKey(id)){
+                        accounts.get(id).display();
+                    } else {
+                        System.out.println("Numéro de compte invalide");
+                    }
+                    break;
+                case 3:
 
-                break;
-            case 5:
+                    break;
+                case 4:
+                    return;
+                case 5:
 
-                break;
-            default:
-                break;
+                    break;
+                default:
+                    System.out.println("Veuillez choisir une option valide");
+                    break;
+            }
         }
     }
 }
